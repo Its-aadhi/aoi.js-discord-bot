f176103c64e376268bd78cccc9463fc826e00257
Aadhi <95705120+Its-aadhi@users.noreply.github.com>
2025-02-13 14:05:00 -0500

    Update roleinfo.js

--- commands/slash/roleinfo.js
+++ commands/slash/roleinfo.js
@@ -4,7 +4,7 @@ module.exports = {
     prototype: "slash",
     code: `
     $interactionReply[Role Name: $roleName[$mentionedRoles[1]]
-    Role ID: $mentionedRoles[1]
+    Role ID: $mentionedRoles[1] 
     Role Color: $roleColor[$mentionedRoles[1]]
     Role Members: $roleMembersCount[$mentionedRoles[1]]]
     `
